4804d53ed89c30cd39f9cbac5396cb1c08e5022b
= <dogukan@kodgemisi.com>
2019-07-16 09:50:35 +0300

    Creating profanity register class

--- src/main/java/com/dogukanhan/profanity/ProfanityRegister.java
+++ src/main/java/com/dogukanhan/profanity/ProfanityRegister.java
@@ -1,133 +1,113 @@
 package com.dogukanhan.profanity;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Properties;
-import java.util.ResourceBundle;
 
 public class ProfanityRegister {
 
-	private Locale[] localeToLoad;
-	private static final String FILE_PATH = "/data/";
-	private static final String FILE_NAME = "profanity_list_";
-	private static final String FILE_EXTENSION = ".txt";
-	private final Map<Locale, HashSet<String>> map = new HashMap<Locale, HashSet<String>>();
-	private static final String supported = "tr,en";
-	private static boolean staticLoad = false;
-	private BufferedReader br;
-	private static final ProfanityRegister instance = new ProfanityRegister();
+    private Locale[] localeToLoad;
+    private static final String FILE_PATH = "/data/";
+    private final Map<Locale, HashSet<String>> map;
+    private final HashSet<String> globalBanList;
+    private BufferedReader br;
+    private static ProfanityRegister instance;
 
-	
-	public static ProfanityRegister getInstance() {
-		return instance;
-	}
-	
-	static {
-		URL rs = ProfanityRegister.class.getClassLoader().getResource("./profanity.properties");
-		if (rs != null) {
-			System.out.println("Configurating ProfanityRegister");
-			File file = new File(rs.getFile());
-			Properties prop = new Properties();
-			try {
-				prop.load(new FileReader(file));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			staticLoad = Boolean.parseBoolean((prop.getProperty("profanity.static_load", "false").toString()));
-			if (staticLoad) {
-				String includes = prop.getProperty("profanity.static_include", "all");
-				if ("all".equals(includes)) {
-					includes = supported;
-				}
-				if (includes.contains(",")) {
-					String[] listOfIncludes = includes.split(",");
-					for (int i = 0; i < listOfIncludes.length; i++) {
-						String string = listOfIncludes[i];
-						instance.loadFile(new Locale(string));
-					}
-				}
-//			Enumeration<Object> keys = prop.keys();
-//			while(keys.hasMoreElements()) {
-//				String key = (String) keys.nextElement();
-//				if(!"profanity.static_load".equals(key) && key.contains("static_inlude")) {
-//				
-//					
-//				}
-//			}
+    private ProfanityRegister() {
 
-			}
-//        String[] files = file.list();
-//        for (int i = 0; i < files.length; i++) {
-//			String string = files[i];
-//			System.out.println(string);
-//		}
-		}
-	}
+        this.map = new HashMap<Locale, HashSet<String>>();
+        this.globalBanList = new HashSet<String>();
 
-	private void loadFile(Locale locale) {
-		HashSet<String> set = new HashSet<String>();
-		map.put(locale, set);
-		InputStream file = ProfanityRegister.class
-				.getResourceAsStream(FILE_PATH + FILE_NAME + locale.toString() + FILE_EXTENSION);
-		br = new BufferedReader(new InputStreamReader(file));
-		String st;
-		try {
-			while ((st = br.readLine()) != null) {
-				set.add(st);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
-	public void loadLocales() {
-		if (localeToLoad != null) {
-			for (int i = 0; i < localeToLoad.length; i++) {
-				loadFile(localeToLoad[i]);
-			}
-		} else
-			throw new NullPointerException("Please set which locale files will be loaded");
-	}
+    public static void main(String args[]) {
+        System.out.println(ProfanityRegister.getRegister().isProfanityFromFile(Locale.ENGLISH, "pussy"));
+    }
 
-	public boolean isProfanity(Locale locale, String word) {
-		HashSet<String> set = map.get(locale);
-		if (set == null)
-			throw new IllegalArgumentException("Profanity " + locale + " havent loaded");
-		return set.contains(word);
-	}
+    public static synchronized ProfanityRegister getRegister() {
 
-	public boolean isProfanityFromFile(Locale locale, String word) {
-		try {
-			InputStream file = ProfanityRegister.class
-					.getResourceAsStream(FILE_PATH + FILE_NAME + locale.toString() + FILE_EXTENSION);
-			br = new BufferedReader(new InputStreamReader(file));
-			String st;
-			while ((st = br.readLine()) != null) {
-				if (word.equals(st))
-					return true;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+        if (instance == null) {
+            instance = new ProfanityRegister();
+        }
 
-	public ProfanityRegister setLocales(Locale... locales) {
-		this.localeToLoad = locales;
-		return this;
-	}
+        return instance;
+    }
 
+
+    private void loadFile(Locale locale) {
+        HashSet<String> set = new HashSet<String>(globalBanList);
+        map.put(locale, set);
+        InputStream file = ProfanityRegister.class
+                .getResourceAsStream(FILE_PATH + locale.toString());
+        br = new BufferedReader(new InputStreamReader(file));
+        String st;
+        try {
+            while ((st = br.readLine()) != null) {
+                set.add(st);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadLocales() {
+        if (localeToLoad != null) {
+            for (Locale locale : localeToLoad) {
+                loadFile(locale);
+            }
+        } else
+            throw new NullPointerException("Please set which locale files will be loaded");
+    }
+
+    public boolean isProfanity(Locale locale, String word) {
+        HashSet<String> set = map.get(locale);
+        if (set == null)
+            throw new IllegalArgumentException("Profanity " + locale + " haven't loaded");
+        return set.contains(word);
+    }
+
+    public boolean isProfanityFromFile(Locale locale, String word) {
+
+        if (globalBanList.contains(word))
+            return true;
+
+        try {
+            InputStream file = ProfanityRegister.class
+                    .getResourceAsStream(FILE_PATH + locale.toString());
+            br = new BufferedReader(new InputStreamReader(file));
+            String st;
+            while ((st = br.readLine()) != null) {
+                if (word.equals(st))
+                    return true;
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public ProfanityRegister setLocales(Locale... locales) {
+        this.localeToLoad = locales;
+        return this;
+    }
+
+    public ProfanityRegister setGlobalBanListStream(InputStream globalBanListStream) {
+        br = new BufferedReader(new InputStreamReader(globalBanListStream));
+        String st;
+        try {
+            while ((st = br.readLine()) != null) {
+                globalBanList.add(st);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
 }
